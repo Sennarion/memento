9de6e5e4bfcc0c01ac36830bca972770d70cb48b
simbiose <simbiose@simbiose-nb.local>
2025-05-20 12:32:18 +0300

    feat: remove create page

---
+++ src/app/create/page.jsx
@@ -0,0 +1,5 @@
+import { NextResponse } from 'next/server';
+import { db } from '@/firebase-config/config';
+import { doc, setDoc } from 'firebase/firestore';
+
+export default async function Create() {}
